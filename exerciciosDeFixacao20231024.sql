c283be1c0f0206a3505aef917ab7391f9d518cf5
Pandamaximus28409 <102782209+Pandamaximus28409@users.noreply.github.com>
2023-10-24 21:15:36 -0300

    exerciciosDeFixacao20231024.sql

---
+++ exerciciosDeFixacao20231024.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE exercicios_trigger;
+USE exercicios_trigger;
+
+-- Criação das tabelas
+CREATE TABLE Clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Auditoria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    mensagem TEXT NOT NULL,
+    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    estoque INT NOT NULL
+);
+
+CREATE TABLE Pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    produto_id INT,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (produto_id) REFERENCES Produtos(id)
+);
